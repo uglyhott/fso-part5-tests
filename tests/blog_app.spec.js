8c312bb6ba5a7944cb3e6c22a976d7fa530d184d
Liam Blumenstein-Tait <liam.bt@gmail.com>
2026-01-30 13:10:32 +0100

    5.18

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,14 +1,41 @@
-import { expect, test } from "@playwright/test"
+import { expect, test } from '@playwright/test'
+import { loginWith } from './helper'
 
-test.describe("Blog App", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/")
+test.describe('Blog App', () => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'tester',
+        password: 'tested'
+      }
+    })
+
+    await page.goto('/')
   })
 
-  test("Login form is shown", async ({ page }) => {
-    await expect(page.getByRole('heading', { name: "Log in to application" })).toBeVisible()
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Log in to application' })).toBeVisible()
     await expect(page.getByLabel('username')).toBeVisible()
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+
+  test.describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'tester', 'tested')
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'tester', 'wrong')
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong Credentials')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+
+      await expect(page.getByText('Test User logged in')).not.toBeVisible()
+    })
   })
 })
