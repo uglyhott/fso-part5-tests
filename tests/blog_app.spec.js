ebc21b247f05261dd06e02e19a4d4d867deecd0b
Liam Blumenstein-Tait <liam.bt@gmail.com>
2026-01-30 15:34:05 +0100

    5.19

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test'
-import { loginWith } from './helper'
+import { loginWith, createBlog } from './helper'
 
 test.describe('Blog App', () => {
   test.beforeEach(async ({ page, request }) => {
@@ -36,6 +36,20 @@ test.describe('Blog App', () => {
       await expect(errorDiv).toHaveCSS('border-style', 'solid')
 
       await expect(page.getByText('Test User logged in')).not.toBeVisible()
+    })
+  })
+
+  test.describe('When logged in', () => {
+    test.beforeEach(async ({ page }) => {
+      await loginWith(page, 'tester', 'tested')
+    })
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'Test Title', 'Mr Test', 'http://www.test.com')
+      
+      const notificationDiv = page.locator('.notification')
+      await expect(notificationDiv).toContainText('New blog added: Test Title by Mr Test')
+      await expect(notificationDiv).toHaveCSS('border-style', 'solid')
+      await expect(page.getByText('Test Title Mr Test')).toBeVisible()
     })
   })
 })
--- tests/helper.js
+++ tests/helper.js
@@ -4,4 +4,13 @@ const loginWith = async (page, username, password) => {
   await page.getByRole('button', { name: 'Login' }).click()
 }
 
-export { loginWith }
+const createBlog = async (page, title, author, url) => {
+  await page.getByRole('button', { name: 'Create new blog' }).click()
+  await page.getByLabel('Title').fill(title)
+  await page.getByLabel('Author').fill(author)
+  await page.getByLabel('URL').fill(url)
+  await page.getByRole('button', { name: 'create' }).click()
+  await page.getByText(`${title} ${author}`).waitFor()
+}
+
+export { loginWith, createBlog }
